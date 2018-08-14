1484e845dd4b6cfd6a951494bb3686c022ee774b
hebaAtta <38388194+hebaAtta@users.noreply.github.com>
2018-08-14 16:21:50 +0200

    jh

--- src/App.js
+++ src/App.js
@@ -1,53 +1,95 @@
-
+import React from 'react'
  import * as BooksAPI from './BooksAPI'
-import React, { Component } from 'react';
 import './App.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import Home from "./components/Home";
-import Search from "./components/Search";
-import Error from "./components/Error";
-import { getAll, update } from './BooksAPI'
-
+import Books from './Books';
+import BookList from './BookList';
+import Shelf from './Shelf';
+import {BrowserRouter ,Route ,Link} from 'react-router-dom';
+import Search from "./Search";
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 class BooksApp extends React.Component {
-  state = {
-    books: [],
-    showSearchPage: true
+  state ={
+    books:[],
+    current:[],
+    want:[],
+    Read:[],
+    query:'',
+    results: [],
+    searchingpage: false
+  }
 
-};
-refreshBookList = () => {
-        this.setState({
-            myRequestComplete: false,
-            books: []
-        })
 
-        getAll()
-            .then((data) => {
-                this.setState({
-                    books: data,
-                    myRequestComplete: true
-                })
-                this.updateBookShelfAssoc(data);
-            });
-    }
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+        this.setState({ books });
+      });
+  }
 
+  // update books
+  updateBooks = (book, shelf) => {
+      BooksAPI.update(book, shelf).then(() => {
+        this.setState(state => ({
+           books:state.books.filter(b => {
+             if (b.id === book.id) {
+                  return (book.shelf = shelf);
+                }
+                  else {
+               return book;
+             }
+          })
+        }));
+      });
+    };
+
+
+//searchBooks
+searchBooks = query => {
+   this.setState({query, searchingpage: true}, () => {
+     
+     BooksAPI.search(query, 20).then(results => {
+       if (results.error) {
+         this.setState({searchingpage: false, results: []})
+       } else {
+         this.setState({
+           searchingpage: false,
+           results: results.sort(sortBy('title'))
+         })
+       }
+     })
+   })
+ }
+
+clearSearch = () => {
+this.setState({query: '', results: [], searching: false})
+}
 
   render() {
     return (
       <div className="app">
-      <Route exact path="/search" render={() =>(
-        <Search />
-      )} />
       <Route
            exact
            path="/"
            render={() => (
-             <Home/>
+             <BookList
+             state={this.state}
+             update={this.updateBooks}/>
            )}
          />
 
+      <Route exact path="/search" render={() =>(
+       <Search
+       state={this.state}
+       books={this.state.books}
+       searchBooks={this.searchBooks}
+       clearSearch={this.clearSearch}
+       />
+     )} />
+    }
+    />
+
       </div>
-    );
+    )
   }
 }
 
